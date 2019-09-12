eded99e01f38aa2f2da22f90815d45f9b6dbdf6d
Edgar Toll <edjopato@gmail.com>
2019-09-12 11:54:22 +0200

    feat(shop-construction): only change every 3h

--- source/lib/game-logic/shop-construction.ts
+++ source/lib/game-logic/shop-construction.ts
@@ -9,14 +9,16 @@ import {HOUR_IN_SECONDS} from '../math/timestamp-constants'
 import * as dataShopConstruction from '../data/shop-construction'
 import * as wdShops from '../wikidata/shops'
 
+const CHANGE_INTERVAL_IN_SECONDS = HOUR_IN_SECONDS * 3
+
 export async function getCurrentConstructions(now: number): Promise<Construction> {
 	let data = await dataShopConstruction.get()
 	const before = stringify(data)
 
-	const nowHour = Math.floor(now / HOUR_IN_SECONDS) * HOUR_IN_SECONDS
-	if (!data || data.timestamp !== nowHour) {
+	const lastChange = lastConstructionChange(now)
+	if (!data || data.timestamp < lastChange) {
 		data = {
-			timestamp: nowHour,
+			timestamp: lastChange,
 			possibleShops: generatePossibleShops(3)
 		}
 	}
@@ -32,4 +34,12 @@ export async function getCurrentConstructions(now: number): Promise<Construction
 function generatePossibleShops(amount: number): string[] {
 	const allShops = wdShops.allShops()
 	return randomUniqueEntries(allShops, amount)
+}
+
+export function lastConstructionChange(now: number): number {
+	return Math.floor(now / CHANGE_INTERVAL_IN_SECONDS) * CHANGE_INTERVAL_IN_SECONDS
+}
+
+export function nextConstructionChange(now: number): number {
+	return Math.ceil(now / CHANGE_INTERVAL_IN_SECONDS) * CHANGE_INTERVAL_IN_SECONDS
 }
