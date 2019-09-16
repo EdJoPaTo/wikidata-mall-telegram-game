1fd83076f27a4a5c9c13894dde9afd8990994541
Edgar Toll <edjopato@gmail.com>
2019-09-16 17:19:48 +0200

    feat(mainmenu): hint skill queue has place
    before it only hinted skill queue is empty

--- source/menu/index.ts
+++ source/menu/index.ts
@@ -2,6 +2,8 @@ import TelegrafInlineMenu from 'telegraf-inline-menu'
 
 import {Session, Persist} from '../lib/types'
 
+import {canAddToSkillQueue} from '../lib/game-math/skill'
+
 import {buttonText} from '../lib/interface/menu'
 import {emojis} from '../lib/interface/emojis'
 import {infoHeader, labeledFloat} from '../lib/interface/formatted-strings'
@@ -77,7 +79,8 @@ menu.submenu(buttonText(emojis.person, 'menu.employee'), 'employees', employees,
 
 function skillEmoji(ctx: any): string {
 	const {skillQueue} = ctx.session as Session
-	return (skillQueue && skillQueue.length > 0 ? '' : emojis.requireAttention) + emojis.skill
+	const now = Date.now() / 1000
+	return (skillQueue && !canAddToSkillQueue(skillQueue, now) ? '' : emojis.requireAttention) + emojis.skill
 }
 
 menu.submenu(buttonText(skillEmoji, 'menu.skill'), 'skill', skills, {
