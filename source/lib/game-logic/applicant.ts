d1f4686062ab445bf128bd171b895c9763a5b140
Edgar Toll <edjopato@gmail.com>
2019-09-17 13:43:43 +0200

    feat(applicants): talents based on person type

--- source/lib/game-logic/applicant.ts
+++ source/lib/game-logic/applicant.ts
@@ -1,4 +1,4 @@
-import gaussian from 'gaussian'
+import gaussian, {Gaussian} from 'gaussian'
 import randomItem from 'random-item'
 
 import {Person, Talents, TALENTS, PersonType} from '../types/people'
@@ -17,6 +17,7 @@ export function createApplicant(skills: Skills, now: number): Person {
 
 	const retirementRandom = Math.random()
 	const type: PersonType = retirementRandom > 0.6 ? 'refined' : 'temporary'
+	const talentDistribution = type === 'refined' ? distributionRefined : distributionTemporary
 
 	const retirement = daysUntilRetirement(skills)
 	const retirementDays = randomBetween(retirement.min, retirement.max, retirementRandom)
@@ -27,29 +28,40 @@ export function createApplicant(skills: Skills, now: number): Person {
 		type,
 		hobby: randomItem(wdShops.allShops()),
 		retirementTimestamp,
-		talents: randomTalents()
+		talents: randomTalents(talentDistribution)
 	}
 }
 
 const MINIMAL_TALENT = 0.001
-const talentDistribution = gaussian(1.1, 0.06)
+const distributionRefined = gaussian(1.25, 0.04)
+const distributionTemporary = gaussian(1.1, 0.01)
 /* DEBUG
-console.log('talentDistribution', talentDistribution.mean, talentDistribution.variance)
-console.log('talentDistribution probability', '<0  :', talentDistribution.cdf(MINIMAL_TALENT))
-console.log('talentDistribution probability', '<0.2:', talentDistribution.cdf(0.2))
-console.log('talentDistribution probability', '>1  :', 1 - talentDistribution.cdf(1))
-console.log('talentDistribution probability', '>1.2:', 1 - talentDistribution.cdf(1.2))
-console.log('talentDistribution probability', '>1.5:', 1 - talentDistribution.cdf(1.5))
-console.log('talentDistribution probability', '>1.8:', 1 - talentDistribution.cdf(1.8))
-console.log('talentDistribution probability', '>2  :', 1 - talentDistribution.cdf(2))
-console.log('talentDistribution probability', '>2.5:', 1 - talentDistribution.cdf(2.5))
-console.log('talentDistribution all negative', talentDistribution.cdf(1) ** TALENTS.length)
+debugDistribution('before', gaussian(1.1, 0.06))
+debugDistribution('refined', distributionRefined)
+debugDistribution('temporary', distributionTemporary)
+function debugDistribution(name: string, distribution: Gaussian): void {
+
+	console.log('debugDistribution', name, distribution.mean, distribution.variance)
+	console.log('kinda min', distribution.ppf(0.0005))
+	console.log('kinda max', distribution.ppf(0.9995))
+	console.log('probability', '<0  :', distribution.cdf(MINIMAL_TALENT))
+	console.log('probability', '<0.2:', distribution.cdf(0.2))
+	console.log('probability', '>1  :', 1 - distribution.cdf(1))
+	console.log('probability', '>1.2:', 1 - distribution.cdf(1.2))
+	console.log('probability', '>1.4:', 1 - distribution.cdf(1.4))
+	console.log('probability', '>1.6:', 1 - distribution.cdf(1.6))
+	console.log('probability', '>1.8:', 1 - distribution.cdf(1.8))
+	console.log('probability', '>2  :', 1 - distribution.cdf(2))
+	console.log('probability', '>2.5:', 1 - distribution.cdf(2.5))
+	console.log('all negative', distribution.cdf(1) ** TALENTS.length)
+	console.log()
+}
 /**/
 
-function randomTalents(): Talents {
+function randomTalents(distribution: Gaussian): Talents {
 	const talents: any = {}
 	for (const t of TALENTS) {
-		const factor = talentDistribution.ppf(Math.random())
+		const factor = distribution.ppf(Math.random())
 		talents[t] = Math.max(MINIMAL_TALENT, factor)
 	}
 
