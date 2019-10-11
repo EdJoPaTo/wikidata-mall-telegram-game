d2615d156c62a1ec043a62dff8f4b1a52aebd88d
Edgar Toll <edjopato@gmail.com>
2019-10-11 11:41:30 +0200

    fix(mall): remove donation
    production is now free -> no need for a donation

--- source/menu/mall/index.ts
+++ source/menu/mall/index.ts
@@ -10,7 +10,6 @@ import {mallMemberAmountWithinLimits} from '../../lib/game-math/mall'
 import {applicantButtonEmoji} from '../../lib/interface/applicants'
 import {buttonText, menuPhoto} from '../../lib/interface/menu'
 import {emojis} from '../../lib/interface/emojis'
-import {formatFloat} from '../../lib/interface/format-number'
 import {infoHeader, labeledFloat} from '../../lib/interface/formatted-strings'
 import {mallMoji, hintIncorrectPeopleAmount} from '../../lib/interface/mall'
 
@@ -19,9 +18,6 @@ import {helpButtonText, createHelpMenu} from '../help'
 import applicantsMenu from './applicants'
 import attractionMenu from './attraction'
 import productionMenu from './production'
-
-const DONATION_AMOUNT = 100
-const DONATION_PERSONAL_FACTOR = 5
 
 async function menuText(ctx: any): Promise<string> {
 	const {__wikibase_language_code: locale} = ctx.session as Session
@@ -98,46 +94,6 @@ function mallProductionRequiresAttention(ctx: any): boolean {
 
 menu.submenu(buttonText(emojis.production, 'mall.production', {requireAttention: mallProductionRequiresAttention}), 'production', productionMenu, {
 	hide: hideWhenMemberAmountNotCorrect
-})
-
-menu.button(buttonText(emojis.currencyMall, 'mall.donation'), 'donate', {
-	hide: (ctx: any) => {
-		const {mall} = ctx.persist as Persist
-		if (!mall || !mallMemberAmountWithinLimits(mall)) {
-			return true
-		}
-
-		const session = ctx.session as Session
-		if (session.money < DONATION_AMOUNT * DONATION_PERSONAL_FACTOR * 2) {
-			return true
-		}
-
-		return mall.money >= DONATION_AMOUNT * 10
-	},
-	doFunc: async (ctx: any) => {
-		const session = ctx.session as Session
-		if (session.money < DONATION_AMOUNT * 2) {
-			return
-		}
-
-		const {mall} = ctx.persist as Persist
-		if (!mall) {
-			throw new Error('user not part of a mall')
-		}
-
-		session.money -= DONATION_AMOUNT * DONATION_PERSONAL_FACTOR
-		mall.money += DONATION_AMOUNT
-
-		let text = ''
-		text += emojis.person
-		text += ' → '
-		text += formatFloat(DONATION_AMOUNT * DONATION_PERSONAL_FACTOR) + emojis.currency
-		text += ' → '
-		text += formatFloat(DONATION_AMOUNT) + emojis.currencyMall
-		text += ' → '
-		text += emojis.mall
-		return ctx.answerCbQuery(text)
-	}
 })
 
 menu.urlButton(
