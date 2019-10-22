eab2362732d987046c3a2cac63bd5a438d131811
Edgar Toll <edjopato@gmail.com>
2019-10-22 15:28:12 +0200

    feat(bot-stats): show more stats

--- source/menu/bot-stats.ts
+++ source/menu/bot-stats.ts
@@ -3,9 +3,13 @@ import TelegrafInlineMenu from 'telegraf-inline-menu'
 import {Person} from '../lib/types/people'
 import {Session} from '../lib/types'
 
+import * as userMalls from '../lib/data/malls'
 import * as userSessions from '../lib/data/user-sessions'
 import * as userShops from '../lib/data/shops'
+import * as wdAttractions from '../lib/wikidata/attractions'
 import * as wdNames from '../lib/wikidata/name'
+import * as wdProduction from '../lib/wikidata/production'
+import * as wdSets from '../lib/wikidata/sets'
 import * as wdShops from '../lib/wikidata/shops'
 
 import {buttonText, menuPhoto} from '../lib/interface/menu'
@@ -28,6 +32,9 @@ async function menuText(ctx: any): Promise<string> {
 	text += labeledInt(ctx.wd.r('product.product'), wdShops.allProductsAmount())
 	text += labeledInt(ctx.wd.r('stat.name.given'), wdNames.getGivenNames().length)
 	text += labeledInt(ctx.wd.r('stat.name.family'), wdNames.getFamilyNames().length)
+	text += labeledInt(ctx.wd.r('mall.production'), wdProduction.getProducts().length)
+	text += labeledInt(ctx.wd.r('mall.attraction'), Object.keys(wdAttractions.all()).length)
+	text += labeledInt(ctx.wd.r('mall.disaster'), wdSets.get('disaster').length)
 
 	text += '\n'
 	text += '*'
@@ -39,8 +46,11 @@ async function menuText(ctx: any): Promise<string> {
 	const allShops = Object.values(allShopsDict).flat()
 	const allProducts = allShops.flatMap(o => o.products)
 	const allEmployees = allShops.flatMap(o => Object.values(o.personal) as Person[])
+	const allMallsDict = await userMalls.getAll()
+	const allMalls = Object.values(allMallsDict)
 
 	text += labeledInt(ctx.wd.r('stat.player'), userSessions.getRaw().length)
+	text += labeledInt(ctx.wd.r('menu.mall'), allMalls.length)
 	text += labeledInt(ctx.wd.r('menu.shop'), allShops.length)
 	text += labeledInt(ctx.wd.r('menu.employee'), allEmployees.length)
 	text += labeledInt(ctx.wd.r('product.product'), allProducts.length)
