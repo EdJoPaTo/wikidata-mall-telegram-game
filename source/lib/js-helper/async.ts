88930a24c12221002e9f9cf7b12bb646dc634301
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-06-11 01:15:09 +0200

    refactor: update wikidata-sdk-got

--- source/lib/js-helper/async.ts
+++ source/lib/js-helper/async.ts
@@ -4,8 +4,8 @@
  * Run the function multiple times with the arguments in order to get all the results over time.
  * The results are concatted into the result array.
  */
-export async function stagedAsync<Argument, Result>(func: (arg: Argument) => Promise<Result[]>, args: readonly Argument[], concurrent = 10): Promise<Result[]> {
-	const results: Result[][] = []
+export async function stagedAsync<Argument, Result>(func: (arg: Argument) => Promise<readonly Result[]>, args: readonly Argument[], concurrent = 10): Promise<Result[]> {
+	const results: Array<readonly Result[]> = []
 
 	while (results.length < args.length) {
 		const stepResults = await Promise.all(
