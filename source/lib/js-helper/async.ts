ffedf9f17ee1f410517fd6c17671f6e4132dedd8
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-06-17 00:16:12 +0200

    fix(async): reduce default concurrency
    doing less things in parallel results hopefully in less often wikidata
    request drops

--- source/lib/js-helper/async.ts
+++ source/lib/js-helper/async.ts
@@ -4,7 +4,7 @@
  * Run the function multiple times with the arguments in order to get all the results over time.
  * The results are concatted into the result array.
  */
-export async function stagedAsync<Argument, Result>(func: (arg: Argument) => Promise<readonly Result[]>, args: readonly Argument[], concurrent = 10): Promise<Result[]> {
+export async function stagedAsync<Argument, Result>(func: (arg: Argument) => Promise<readonly Result[]>, args: readonly Argument[], concurrent = 4): Promise<Result[]> {
 	const results: Array<readonly Result[]> = []
 
 	while (results.length < args.length) {
